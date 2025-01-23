24139c4a2b5db9e1eade6514c5d18daa22a7ceb0
FrostLua <139758993+FrostLua@users.noreply.github.com>
2025-01-23 17:58:39 +0100

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+local gid = game.GameId;
+if gid == 18307809542 or gid == 2142948266 then -- Project Slayers
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Scripts/main/ProjectSlayers/Script.lua"))();
+elseif gid == 6035872082 then -- Rivals
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Scripts/main/Rivals.lua"))();
+end;
