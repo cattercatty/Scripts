bf451bce234a9e5886bf37206abeaaf427767691
FrostLua <139758993+FrostLua@users.noreply.github.com>
2025-04-12 23:15:12 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,4 +3,6 @@ if gid == 18307809542 or gid == 2142948266 then -- Project Slayers
   loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Scripts/main/ProjectSlayers/Script.lua"))();
 elseif gid == 6035872082 then -- Rivals
   loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Scripts/main/Rivals.lua"))();
+elseif gid == 7074860883 then --arise crossover
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Scripts/refs/heads/main/AriseCrossover/Script.luau"))();
 end;
