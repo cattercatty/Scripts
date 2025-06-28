be55d5a1f90895a4f6c728e3e4d0bb86c1f61e55
cattercatty <139758993+cattercatty@users.noreply.github.com>
2025-06-28 19:28:27 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,8 @@
 local gid = game.GameId;
 if gid == 18307809542 or gid == 2142948266 then -- Project Slayers
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Scripts/main/ProjectSlayers/Script.lua"))();
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/cattercatty/Scripts/main/ProjectSlayers/Script.lua"))();
 elseif gid == 6035872082 then -- Rivals
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Scripts/main/Rivals.lua"))();
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/cattercatty/Scripts/main/Rivals.lua"))();
 elseif gid == 7074860883 then --arise crossover
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Scripts/refs/heads/main/AriseCrossover/Script.luau"))();
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/cattercatty/Scripts/refs/heads/main/AriseCrossover/Script.luau"))();
 end;
