fa6a07365526ddd3cb6b56dec13729901ef611c3
Catty <139758993+cattercatty@users.noreply.github.com>
2025-09-24 17:56:58 +0200

    Update Script.lua

--- ProjectSlayers/Script.lua
+++ ProjectSlayers/Script.lua
@@ -69,7 +69,7 @@ end;
 
 if not getgenv().script_key or not script_key and isfolder and isfolder("Frosties") and isfile and isfile("Frosties/Key.txt") and readfile then
 	local key = isfile and isfile("Frosties/Key.txt") and readfile and readfile("Frosties/Key.txt");
-	ret = #key == 32 and checkKey(key) or false;
+	ret = key and #key == 32 and checkKey(key) or false;
 	if ret == true then
 		return;
 	end;
